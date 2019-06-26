f157a696bafeba94d788644bb88374d55c107e58
SamuelV3loso <samuelveloso42@gmail.com>
2019-06-25 21:53:03 -0300

    added types to gramatica

--- lexico.js
+++ lexico.js
@@ -1,6 +1,11 @@
 const gramatica = [
     {type: 'number', regex: '^[0-9]$'},
     {type: 'variable', regex: '^[A-Z]$'},
+    {type: "conditional", regex: '^[if]$'},
+    {type: "open-bracket", regex: '^[(]$'}, //opens brackets
+    {type: "close-bracket", regex: '^[)]$'}, //close brackets
+    {type: "open-key", regex: '^[{]$'}, //opens keys
+    {type: "close-key", regex: '^[}]$'}, //close keys
 ];
 const lexico = code => {
     let str = '';
