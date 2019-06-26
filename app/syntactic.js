e7ce10ea11e26c40565077b254b7f0838f4d9f70
Samuel Veloso <samuelveloso42@gmail.com>
2019-06-26 09:23:17 -0300

    fix scope logical

--- app/syntactic.js
+++ app/syntactic.js
@@ -1,13 +1,11 @@
 const syntactic = (code, dictionary) => {
   
-    let isEscope = false;
     let escope = 1;
-
     for(let i = 0; i< code.length; i++){
-        if(code[i]['type'] == 'variable' && !isEscope) code[i]['nivel-escope'] = escope;
-        else if(code[i]['type'] == 'scope-open' && !isEscope) ++escope;
-        else if(code[i]['type'] == 'scope-close' && !isEscope) --escope;   
+        if(code[i]['type'] == 'variable') code[i]['nivel-escope'] = escope;
+        else if(code[i]['type'] == 'scope-open') ++escope;
+        else if(code[i]['type'] == 'scope-close') --escope;   
     }
-
+    
 }
 module.exports = syntactic;
