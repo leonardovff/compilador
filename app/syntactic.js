ad8f49a41bef28bee324ec07227e6ce9b9932028
Samuel Veloso <samuelveloso42@gmail.com>
2019-06-26 09:07:16 -0300

    added scope nivels

--- app/syntactic.js
+++ app/syntactic.js
@@ -1,3 +1,13 @@
 const syntactic = (code, dictionary) => {
+  
+    let isEscope = false;
+    let escope = 1;
+
+    for(let i = 0; i< code.length; i++){
+        if(code[i]['type'] == 'variable' && !isEscope) code[i]['nivel-escope'] = escope;
+        else if(code[i]['type'] == 'scope-open' && !isEscope) ++escope;
+        else if(code[i]['type'] == 'scope-close' && !isEscope) --escope;   
+    }
+
 }
 module.exports = syntactic;
